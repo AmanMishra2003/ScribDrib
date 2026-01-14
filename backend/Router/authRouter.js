7ad5077173aa75b15c4db0519bbf334c4044a6e1
Aman Mishra <mishraaman2021@gmail.com>
2026-01-15 00:47:21 +0530

    initial structure created

---
+++ backend/Router/authRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+//routes
+//login route
+
+
+//signUp route
+
+
+// logout route 
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 
 const app = express(); //initializing app
@@ -9,6 +10,22 @@ const port  = 3000; //port name
 app.use(cors()); //cross origin resourse sharing
 
 app.use(express.json());  //parse JSON object to JS Object
+
+//Database connection
+const mongodbPath = process.env.DATABASEURL 
+mongoose.connect(mongodbPath).then(()=>{
+    console.log("connection successfull!")
+}).catch((err)=>{
+    console.log(err)
+})
+
+//router imports
+const HomeRouter = require('./Router/homeRouter');
+const AuthRouter = require('./Router/authRouter');
+
+app.use('/',HomeRouter);
+app.use('/auth',AuthRouter);
+
 
 //error handler
 app.use((err,req,res,next)=>{
