78314740adad0305bbd26412839171246013ebaa
Aman Mishra <mishraaman2021@gmail.com>
2026-01-15 22:56:01 +0530

    tailwindConfig animation

--- frontend/ScribDrib/vite.config.js
+++ frontend/ScribDrib/vite.config.js
@@ -1,13 +1,33 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import animate from "tailwindcss-animate";
+
+>>>>>>> 2991dad (tailwindConfig animation)
 
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+=======
+  plugins: [react(),
+       tailwindcss(),
+  ],
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,"./src")
+    }
+  }
+>>>>>>> 2991dad (tailwindConfig animation)
 });
