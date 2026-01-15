3e6f3b5ea6f3122540aa40c2872e988aeb006fcf
AYUSH PRATAP SINGH <ayushpratapsingh@AYUSHs-MacBook-Air.local>
2026-01-15 21:56:06 +0530

    setup react with routing

--- frontend/ScribDrib/src/App.jsx
+++ frontend/ScribDrib/src/App.jsx
@@ -1,34 +1,26 @@
-
 import './App.css'
-import {BrowserRouter,Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
-//HomePage
+// HomePage
 import HomePage from './components/Home/HomePage'
-//Auth Components
+
+// Auth Components
 import AuthComponent from './components/AuthComponents/AuthComponent'
 import Login from './components/AuthComponents/Login'
 import Signup from './components/AuthComponents/Signup'
 
-
 function App() {
-
   return (
-    // create Browsing Routers 
-    <BrowserRouter>
+    <Routes>
+      {/* Home page */}
+      <Route path='/' element={<HomePage />} />
 
-      {/* all routes inside Routes tag */}
-      <Routes>
-        <Route path='/' element={<HomePage/>} /> {/* home page */}
-
-        {/* auth Path */}
-        <Route path='/auth' element={<AuthComponent/>}> {/* auth component have outlet for login and signup page */}
-            <Route path='login' element={<Login/>} /> {/* login page */}
-            <Route path='signup' element={<Signup/>} /> {/* signup page */}
-        </Route>
-
-      </Routes>
-
-    </BrowserRouter>
+      {/* Auth layout */}
+      <Route path='/auth' element={<AuthComponent />}>
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+      </Route>
+    </Routes>
   )
 }
 
