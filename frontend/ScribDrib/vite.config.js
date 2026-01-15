719e59c211a65dba5710e956a28a7d38a53b9de5
Aman Mishra <mishraaman2021@gmail.com>
2026-01-15 23:10:51 +0530

    fix: remove invalid tw-animate-css dependency and update Vite config

--- frontend/ScribDrib/vite.config.js
+++ frontend/ScribDrib/vite.config.js
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 import animate from "tailwindcss-animate";
 
->>>>>>> 2991dad (tailwindConfig animation)
-
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-=======
-  plugins: [react(),
-       tailwindcss(),
-  ],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src")
-    }
-  }
->>>>>>> 2991dad (tailwindConfig animation)
 });
